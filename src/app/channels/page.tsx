55710c369f23ee8e04115e4b956cebd19e0b8c61
jiangwei24680 <jiangwei24680@live.com>
2025-08-30 20:08:48 +0800

    Create page.tsx

---
+++ src/app/channels/page.tsx
@@ -0,0 +1,8 @@
+'use client'
+
+export const dynamic = 'force-static'
+export const runtime = 'edge'
+
+export default function ChannelsPage() {
+  return <div className="p-6 text-xl">✅ 构建测试页面</div>
+}
