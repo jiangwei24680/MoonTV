7f124c5a131017a804b499230abd56e94742eaf9
jiangwei24680 <jiangwei24680@live.com>
2025-08-30 09:06:12 +0800

    Update page.tsx

--- src/app/douban/page.tsx
+++ src/app/douban/page.tsx
@@ -14,6 +14,7 @@ import DoubanCustomSelector from '@/components/DoubanCustomSelector';
 import DoubanSelector from '@/components/DoubanSelector';
 import PageLayout from '@/components/PageLayout';
 import VideoCard from '@/components/VideoCard';
+import LiveChannelList from '@/components/LiveChannelList';
 
 function DoubanPageClient() {
   const searchParams = useSearchParams();
